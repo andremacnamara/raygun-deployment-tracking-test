9f4bd7dc5a251094b8efc79c766b5788d0a20fd4
Andre MacNamara <5852796+andremacnamara@users.noreply.github.com>
2024-03-29 16:28:54 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started by editing &nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <button onClick={handleClick}>Click</button>
