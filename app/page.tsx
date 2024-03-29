c7ba62ff6cf898f1023df72d19505e64fe2c9492
Andre MacNamara <5852796+andremacnamara@users.noreply.github.com>
2024-03-29 15:02:42 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,11 @@ export default function Home() {
       console.log("Script test.");
     }
   }, []);
+
+  const handleClick = () => {
+    throw new Error("This is a test exception");
+  };
+  
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -18,6 +23,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
+        <button onClick={handleClick}>Click</button>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
