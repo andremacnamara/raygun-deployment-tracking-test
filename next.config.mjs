66e051728ff10f268141c94fac03cff495be29e2
Andre MacNamara <5852796+andremacnamara@users.noreply.github.com>
2024-04-26 11:07:06 +0100

    Tetst source maps

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
+const { RaygunWebpackPlugin } = require("@raygun.io/webpack-plugin");
+
+const raygunPlugin = new RaygunWebpackPlugin({
+  applicationId: "2bw3ch4",
+  patToken: process.env.RAYGUN_PAT_TOKEN, // Your Raygun Personal Access Token
+  baseUri: "https://raygun-deployment-tracking-test.vercel.app/",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Define environment variables
   env: {
     RAYGUN_API_KEY: process.env.RAYGUN_API_KEY,
+
     // Add more environment variables as needed
   },
+};
+
+module.exports = {
+  plugins: [raygunPlugin],
 };
 
 export default nextConfig;
